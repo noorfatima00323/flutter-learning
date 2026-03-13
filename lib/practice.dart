ba20c6d42c6a8abaa72f869f76a216e502839752
Noor <noorfatima00323@gmail.com>
2026-03-13 21:35:56 +0500

    if else condition

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,7 +1,15 @@
 import 'dart:io';
 
 void main() {
+  stdout.write('Welcome to Dart!');
   stdout.write('Enter your name:');
   var name = stdin.readLineSync();
   stdout.writeln("Hello, $name!");
+  stdout.write('Enter a number: ');
+  int number = int.parse(stdin.readLineSync()!);
+  if (number > 5) {
+    stdout.write("Number is greater than 5");
+  } else {
+    stdout.write("Number is 5 or less");
+  }
 }
