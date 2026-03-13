657d17644803108feee8185cca1f3ab7280430ce
Noor <noorfatima00323@gmail.com>
2026-03-13 21:58:51 +0500

    comments

--- lib/practice.dart
+++ lib/practice.dart
@@ -7,21 +7,24 @@ void main() {
   stdout.writeln("Hello, $name!");
   stdout.write('Enter a number: ');
   int number = int.parse(stdin.readLineSync()!);
+  // if else statement
   if (number > 5) {
     stdout.write("Number is greater than 5");
   } else {
     stdout.writeln("Number is 5 or less");
   }
+  // while loop
   int i = 1;
   while (i <= 5) {
     stdout.writeln(i);
     i++;
   }
+  //for loop
   for (int j = 1; j <= 5; j++) {
     stdout.writeln('printing $j');
   }
+  // do while loop
   int k = 1;
-
   do {
     stdout.writeln(k);
     k++;
